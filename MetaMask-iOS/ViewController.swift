effc5724e1a6136c8602f2b20cd530756365f7e4
Ajay <ajay.kumar@iphtechnologies.com>
2023-08-04 16:45:23 +0530

    Redirect Chain ID

--- MetaMask-iOS/ViewController.swift
+++ MetaMask-iOS/ViewController.swift
@@ -1,100 +1,45 @@
 import UIKit
-import metamask_ios_sdk
-import Combine
+import Glaip
 
 class ViewController: UIViewController {
     
-    private var ethereum: Ethereum {
-        return MetaMaskSDK.shared.ethereum
-    }
-    private var cancellables: Set<AnyCancellable> = []
-    private var chainId: String?
-    private var balance: String?
+    var glaip = Glaip(
+        title: "Glaip Demo App",
+        description: "Demo app to demonstrate Web3 login",
+        supportedWallets: [.Rainbow]
+    )
+  
+    @IBOutlet weak var chainIDLabel: UILabel!
     
-    private let chainIdLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private let balanceLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        return label
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-        connectToMetaMask()
+        
+      
+        
+        
     }
     
-    private func setupViews() {
-        view.backgroundColor = .white
+    
+    @IBAction func linkWalletButton(_ sender: Any) {
         
-        view.addSubview(chainIdLabel)
-        view.addSubview(balanceLabel)
+        connectToMetaMask()
         
-        chainIdLabel.translatesAutoresizingMaskIntoConstraints = false
-        chainIdLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
-        chainIdLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        chainIdLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         
-        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
-        balanceLabel.topAnchor.constraint(equalTo: chainIdLabel.bottomAnchor, constant: 20).isActive = true
-        balanceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        balanceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
     }
     
     private func connectToMetaMask() {
-        let dapp = Dapp(name: "Baby colouring book for kids", url: "https://apps.apple.com/in/app/baby-colouring-book-for-kids/id1129192831")
-        ethereum.connect(dapp)
-        
-        // Fetch Chain ID
-        ethereum.$chainId
-       // ethereum.requestChainId()
-            .sink(receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .failure(let error):
-                    print("\(error.localizedDescription)")
-                case .finished:
-                    break
-                }
-            }, receiveValue: { [weak self] result in
-                self?.chainId = result
+        glaip.loginUser(type: .MetaMask) { result in
+            switch result {
+            case .success(let user):
+                print(user.wallet.address)
                 DispatchQueue.main.async {
-                    self?.chainIdLabel.text = "Chain ID: \(result)"
+                    self.chainIDLabel.text = "Chain ID: \(user.wallet.address)"
                 }
-            })
-            .store(in: &cancellables)
-        
-        // Fetch Account Balance
-        let parameters: [String] = [
-            ethereum.selectedAddress, // address to check for balance
-            "latest" // "latest", "earliest" or "pending" (optional)
-        ]
-        
-        let getBalanceRequest = EthereumRequest(
-            method: .ethGetBalance,
-            params: parameters)
-        
-        if let balanceCancellable = ethereum.request(getBalanceRequest)?
-            .sink(receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .failure(let error):
-                    print("\(error.localizedDescription)")
-                case .finished:
-                    break
-                }
-            }, receiveValue: { [weak self] result in
-                if let balance = result as? String {
-                    self?.balance = balance
-                    DispatchQueue.main.async {
-                        self?.balanceLabel.text = "Account Balance: \(balance)"
-                    }
-                }
-            }) {
-            cancellables.insert(balanceCancellable)
+            case .failure(let error):
+                print(error)
+            }
         }
     }
+
 }
